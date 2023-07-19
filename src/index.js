41ad9e526f96dff11bbe52bc51f8327e785e405c
Brandon Ting <brandon1221t@gmail.com>
2023-07-19 23:54:18 +0800

    implementation of get TopUsageMons and setup basic jsdoc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
+
+/**
+ * Description placeholder
+ * @date 2023/7/19 - 下午11:13:00
+ * @typedef {Object<string, number>} smogonUsageObjectStructure 
+ * @typedef {Object<string, { Moves: smogonUsageObjectStructure, Abilities: smogonUsageObjectStructure, Teammates: smogonUsageObjectStructure, usage: number, Items: smogonUsageObjectStructure, Spreads: smogonUsageObjectStructure }>} smogonRawData
+ */
+
+/**
+ * Accepts format, rating, and period to target specific JSON for analyzing.
+ * @date 2023/7/19 - 下午10:52:08
+ *
+ * @class smogonDataAnalyzer
+ * @classdesc Core class that provides methods for cleaner information of chaos data from smogon
+ */
 class smogonDataAnalyzer {
-    constructor(format, rating = 0, period) {
-        // TODO add verification
+
+    /**
+     * Creates an instance of smogonDataAnalyzer.
+     * @date 2023/7/19 - 下午11:02:44
+     *
+     * @constructor
+     * @param {Object} identifierOption data from Smogon is grouped by the format battles use, the ratings/level players at, and when does the battles take place. 
+     * @param {string} identifierOption.format  The format you want to analyze. ex. gen9vgc2023regulationd
+     * @param {number} [identifierOption.rating] The rating threshold you want to analyze. ex. 0, 1760, etc. 
+     * @param {string} identifierOption.period   The time period you want to analyze, mainly grouped by month. ex. 2023-06;
+     * @param {smogonRawData} [identifierOption.rawData] Data from Smogon chaos json waiting for being anaylzed.
+     */
+    constructor({ format, rating = 0, period, rawData }) {
+        if (!format) {
+            throw new Error('Format is required. Please check folder names at https://www.smogon.com/stats/2023-06/ for reference.')
+        }
+        // if user doesn't provide period, get last month in yyyy-mm format
         if (period) {
             this.period = period
         } else {
@@ -9,10 +39,42 @@ class smogonDataAnalyzer {
             const latestDataMonth = today.toISOString().substring(0, 7)
             this.period = latestDataMonth
         }
-        this.format = format
-        this.rating = rating
 
+        this.format = format;
+        this.rating = rating;
+        if (this.rawData) {
+            this.rawData = rawData
+        }
     }
+
+
+    /**
+     * Check if user actually init the analyzer before doing every analyze.
+     * @date 2023/7/19 - 下午11:06:24
+     */
+
+    /**
+     * Description placeholder
+     * @date 2023/7/19 - 下午11:45:50
+     *
+     * @param {smogonRawData} [rawData]
+     * @returns {rawData is smogonRawData}
+     */
+    #checkRawDataExist(rawData) {
+        if (!rawData) {
+            throw new Error('Please init the instance with data from smogon correctly by load existing JSON data or let the module fetch from smogon with provided info.')
+        }
+        return true
+    }
+
+
+    /**
+     * Standalone init function to load data locally or fetch from smogon asynchronously;
+     * @date 2023/7/19 - 下午11:47:17
+     *
+     * @async
+     * @param {smogonRawData} [rawData]
+     */
     async init(rawData) {
         if (rawData) {
             this.rawData = rawData;
@@ -22,14 +84,39 @@ class smogonDataAnalyzer {
         try {
             const response = await fetch(`${smogonBaseUrl}/${this.period}/chaos/${this.format}-${this.rating}.json`)
             const data = await response.json();
-            this.data = data;
+            this.rawData = data.data;
         } catch (err) {
-            throw Error('Init data from smogon failed. Please check your internect connection.')
+            throw new Error('Init data from smogon failed. Please check your internect connection.')
         }
     }
-    getTopUsagePokemons() {
+
+
+    /**
+     * Description placeholder
+     * @date 2023/7/19 - 下午11:48:50
+     *
+     * @param {number} [size=30]
+     * @param {boolean} [withNumber=true]
+     * @returns {(string | smogonUsageObjectStructure)[]} topUsageList
+     */
+    getTopUsagePokemons(size = 30, withNumber = true) {
+        if (!this.#checkRawDataExist(this.rawData)) return [];
+        return Object.entries(this.rawData)
+            .sort(function (monA, monB) {
+                return monB[1].usage - monA[1].usage
+            })
+            .slice(0, size)
+            .map(function (monEntry) {
+                if (withNumber) {
+                    return {
+                        [monEntry[0]]: monEntry[1].usage
+                    }
+                }
+                return monEntry[0]
+            })
 
     }
+
 
     getUsageOfPokemon() {
 
@@ -55,16 +142,20 @@ class smogonDataAnalyzer {
 
     }
 
-    getUsageOfPokemon() {
-
-    }
 }
 
 // test code 
 (async function () {
-    const tester = new smogonDataAnalyzer('gen9vgc2023regulationd', 1760, '2023-06');
+    const tester = new smogonDataAnalyzer({
+        format: 'gen9vgc2023regulationd',
+        rating: 1760,
+        period: '2023-06'
+    });
     await tester.init();
-    console.log(tester.data)
+    console.log(tester.period)
+    console.log(tester.rawData)
+    const topUsage = tester.getTopUsagePokemons()
+    console.log(topUsage)
 })()
 
 
--- src/utils/utils.js
+++
@@ -1,1 +0,0 @@
-export const smogonBaseUrl = 'https://www.smogon.com/stats/'
