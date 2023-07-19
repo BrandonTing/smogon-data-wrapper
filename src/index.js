462c8c5a28a441ab8293f7ad79f654df8fac18b3
brandonting <brandon1221t@gmail.com>
2023-07-19 14:00:00 +0800

    switch implementation to don't store json locally

--- src/index.js
+++ src/index.js
@@ -1,90 +1,73 @@
-const { mkdirSync, readFileSync } = require('fs');
-const { isFolderExist, saveAllFilesToTargetFolder, getAllFileNamesFromRemoteFolder } = require('./utils/files');
-const path = require('path');
-
-const smogonBaseUrl = 'https://www.smogon.com/stats/'
-
-/**
- * Return Type of syncFromSmogon Funciton 
- * @date 2023/7/8 - 上午10:15:48
- *
- * @typedef {Object} syncResult
- * @property {!boolean} success Whether the wrapper get data from smogon successfully.
- * @property {boolean} [hasUpdate] Whether the sync updates
- * @property {string[]} [updatedFolders] Folders that has get updated
- * @property {string} [latestFolder] Name of the latest folder from smogon
- */
-
-/**
- * Get chaos json from smogon, url: https://www.smogon.com/stats/
- * @async
- * @param {string} targetFolder folder path to store smogon data
- * @returns {Promise<syncResult>} syncResult
- */
-async function syncFromSmogon(targetFolder, rule) {
-    try {
-        const fileNames = (await getAllFileNamesFromRemoteFolder(smogonBaseUrl, /(\d{4}-\d{2}(?:-\w+)?)\/">/)).map(name => name.replace('/">', ''));
-        // check if local file need to update
-        const isDataFolderExist = isFolderExist(targetFolder)
-        if (!isDataFolderExist) {
-            mkdirSync(targetFolder)
+class smogonDataAnalyzer {
+    constructor(format, rating = 0, period) {
+        // TODO add verification
+        if (period) {
+            this.period = period
+        } else {
+            const today = new Date()
+            today.setMonth(today.getMonth() - 1)
+            const latestDataMonth = today.toISOString().substring(0, 7)
+            this.period = latestDataMonth
         }
+        this.format = format
+        this.rating = rating
 
-        fileNames.forEach(async (name, i) => {
-            if (i !== fileNames.length - 1) return
-            const localFolderPath = path.join(targetFolder, name)
-            // const hasLocalFolder = isFolderExist(localFolderPath);
-            // if (hasLocalFolder) return
-            // create folder and save json from chaos
-            const chaosJsonUrl = `${smogonBaseUrl}${name}/chaos/`
-            await saveAllFilesToTargetFolder(localFolderPath, chaosJsonUrl, rule)
-        });
-
-
-        return {
-            success: true,
-            hasUpdate: true,
-            latestFolder: fileNames.slice(-1)[0]
+    }
+    async init(rawData) {
+        if (rawData) {
+            this.rawData = rawData;
+            return;
         }
-    } catch (err) {
-        return {
-            success: false,
+        const smogonBaseUrl = 'https://www.smogon.com/stats/';
+        try {
+            const response = await fetch(`${smogonBaseUrl}/${this.period}/chaos/${this.format}-${this.rating}.json`)
+            const data = await response.json();
+            this.data = data;
+        } catch (err) {
+            throw Error('Init data from smogon failed. Please check your internect connection.')
         }
+    }
+    getTopUsagePokemons() {
+
+    }
+
+    getUsageOfPokemon() {
+
+    }
+
+    getCommonEVsOfPokemon() {
+
+    }
+
+    getCommonItemsOfPokemon() {
+
+    }
+
+    getCommonMovesOfPokemon() {
+
+    }
+
+    getAbilityUsageOfPokemon() {
+
+    }
+
+    getCommonTeammatesOfPokemon() {
+
+    }
+
+    getUsageOfPokemon() {
+
     }
 }
 
+// test code 
+(async function () {
+    const tester = new smogonDataAnalyzer('gen9vgc2023regulationd', 1760, '2023-06');
+    await tester.init();
+    console.log(tester.data)
+})()
 
-/**
- * getUsageRateOfRule get top usage pokemons based on given params 
- * @date 2023/7/9 - 上午11:01:45
- *
- * @param {string} dataFolder path you store json files from smogon
- * @param {string} timeline ex. 2023-06. The period you want info about
- * @param {string} rule ex. gen9vgc2023regulationd. The Battle rule you want info about.
- * @param {number} [size=30] the number of top rankings you want to know;
- * @returns {{[key: string]: number}[]}
- */
-function getUsageRateOfRule(dataFolder, timeline, rule, size = 30) {
-    const filePath = path.join(dataFolder, timeline, `${rule}.json`)
-    const data = require(filePath);
-    const usage = Object.entries(data.data).sort((a, b) => {
-        const [_A, pokemonAData] = a;
-        const [_B, pokemonBData] = b;
-        return pokemonBData.usage - pokemonAData.usage
-    })
-        .slice(0, size)
-        .map(arr => {
-            return {
-                [arr[0]]: arr[1].usage
-            }
-        });
-    return usage
-}
-
-// syncFromSmogon(path.join(__dirname, '..', 'data'), 'gen9vgc2023regulationd')
-// getUsageRateOfRule(path.join(__dirname, '..', 'data'), '2023-06', 'gen9vgc2023regulationd-0')
 
 module.exports = {
-    syncFromSmogon,
-    getUsageRateOfRule
+    smogonDataAnalyzer
 }
---
+++ src/utils/utils.js
@@ -0,0 +1,1 @@
+export const smogonBaseUrl = 'https://www.smogon.com/stats/'
